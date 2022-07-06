57ae3e6c4602cfa166ee702be586fba68b36a4a0
Aditya Kumar <ceo@sameerswain.com>
2022-07-06 12:02:21 +0530

    added index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-//samir 
+const express=require('express');
+const bodyParser=require('body-parser');
+const route=require('./route/route');
+const app=express();
+const mongoose=require('mongoose');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb+srv://samirlohiya909:Lohiya123@samirlohiya.nszppy8.mongodb.net/Project-3group62?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/',route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
+
