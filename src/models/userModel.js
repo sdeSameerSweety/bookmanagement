18cb15fa51d98b4ddeed2db79800804a33ff8ba3
Aditya Kumar <ceo@sameerswain.com>
2022-07-06 12:26:01 +0530

    userModel created

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,47 @@
 //sajala
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema( {
+    title: {
+        type: String,
+        required: true,
+        enum: ["Mr", "Mrs", "Miss"] ,
+        trim: true
+    },
+    name : {
+        type :String,
+        required : true,
+        trim: true
+    },
+    phone : {
+        type : String,
+        required : true,
+        unique : true,
+        trim: true
+    },
+    email : {
+        type :String,
+        required : true,
+        unique : true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        street: {type :String,
+            trim: true
+        },
+        city: {type :String,
+            trim: true
+        },
+        pincode: {type :String,
+            trim: true} //length must be 6
+      }
+
+  
+},{ timestamps: true });
+
+module.exports = mongoose.model('User', userSchema)
