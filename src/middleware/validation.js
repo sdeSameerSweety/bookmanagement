6ed9ecfe40c57a775e331a7c5c797e81d3c6d28d
Aditya Kumar <ceo@sameerswain.com>
2022-07-07 14:00:14 +0530

    modificreateUser

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -12,18 +12,31 @@ const isValidEmail=(mail)=>{
     return true
 }
 
-
+const regex = /\d/;
 const isVerifyString = function (string) {
     return regex.test(string)
 }
 
 const validateMobile = function(number) {
-    if(/^[0-9]+$/.test(number)) return true
+    let trim = number.trim()
+  var  reg = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
+  return  reg.test(trim) ;
 }
 
+
+const validPasward = function checkPassword(password)
+{
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password);
+}
+
+//^[0-9]+$/
+//^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$
+//^[0-9]+$
+//r'^(+91[-\s]?)?[0]?(91)?[789]\d{9}$'
 // if(/^[1-9][0-9]{5}$/.test(pin))
 
-module.exports={isValidEmail,isEmpty,isValid,isVerifyString,validateMobile}
+module.exports={isValidEmail,isEmpty,isValid,isVerifyString,validateMobile,validPasward}
 
 
 
